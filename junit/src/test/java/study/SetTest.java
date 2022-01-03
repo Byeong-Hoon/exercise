17c4654ed1373ce0e158e1373bf86f433def3854
Byeong-Hoon <hbh0762@naver.com>
2022-01-03 11:12:57 +0900

    test: step1 test

--- junit/src/test/java/study/SetTest.java
+++ junit/src/test/java/study/SetTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.AssertTrue.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
@@ -36,7 +36,7 @@ public class SetTest {
     }
 
     @Test
-    void contains(){
+    void contains() {
         assertThat(numbers.contains(1)).isTrue();
         assertThat(numbers.contains(2)).isTrue();
         assertThat(numbers.contains(3)).isTrue();
@@ -46,8 +46,15 @@ public class SetTest {
     @DisplayName("set 내부 값의 존재 여부 확인 - 중복코드 제거")
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
-    void usingContain(int nums){
-        assertTrue(numbers.contains(nums));
+    void set_containsTest(int element) {
+        assertTrue(numbers.contains(element));
     }
-    
+
+    @DisplayName("true, false 값을 같이 리턴한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void set_contains_ture_false(int element, boolean expected){
+        assertThat(numbers.contains(element)).isEqualTo(expected);
+    }
+
 }
